e876a1b123f3fd605c76bafd50a425fdf10b43b9
Muhammad Tamim <mdt40234@gmail.com>
2024-03-22 22:24:49 +0600

    react router install and setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world!!!!!!!!!!!!!
       </h1>
       <div className="rating">
         <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './App';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
